760ad09144388fd6c6f0e2bda5d7a749dd8a7f16
ZoyaaaZoy <rifkyandriansyah444@gmail.com>
2025-07-03 12:48:28 +0700

    Update SEO lagi

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,9 +6,14 @@ export default function Document() {
       <Head />
       <link rel="icon" type="image/png" sizes="32x32" href="/images/logoDark.png" />
       <title>Gesly Ardiansyah</title>
-      <meta property="og:description" content="Gesly Ardiansyah adalah seorang anak yang dibesarkan dilingkungan seni. Seni mengalir di darah saya sedari lahir karna Nenek saya adalah seorang pelakon seni di Jakarta. Saya juga seorang penyanyi, khususnya penyanyi lagu-lagu daerah (Sunda). Saya juga seorang freelance model/talent/muse untuk beberapa produk baju, film pendek, iklan, serta penyambutan tamu diberbagai acara dan lain-lain." />
-      <meta name="keywords" content="Gesly Ardiansyah, Gesly, Ages, Model Bandung" />
-      <meta name="robots" content="index, follow" />
+      <meta name="description" content="Website resmi Gesly Ardiansyah, musisi di Jawabarat, Indonesia. Lihat video TikTok, Spotify, dan karya terbaiknya di sini." />
+      <meta name="keywords" content="Gesly Ardiansyah, musisi, Ages, Gesly, Model, Spotify Indonesia" />
+      <meta name="author" content="Gesly Ardiansyah" />
+      <meta property="og:title" content="Gesly Ardiansyah - Official Site" />
+      <meta property="og:description" content="Official portfolio and media site of Gesly Ardiansyah" />
+      <meta property="og:image" content="/public/images/heroImg.png" />
+      <meta property="og:url" content="https://www.geslyardiansyah.site/" />
+      <meta name="twitter:card" content="summary_large_image" />
       <link rel="canonical" href="https://www.geslyardiansyah.site/" />
       <body className="antialiased">
         <Main />
