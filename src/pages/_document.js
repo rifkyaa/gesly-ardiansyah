e11ead538eadee616815c9d3ec8f3cd10c6af648
ZoyaaaZoy <rifkyandriansyah444@gmail.com>
2025-07-03 12:30:51 +0700

    update SEO

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,9 +6,10 @@ export default function Document() {
       <Head />
       <link rel="icon" type="image/png" sizes="32x32" href="/images/logoDark.png" />
       <title>Gesly Ardiansyah</title>
-      <meta property="og:description" content="Gesly Ardiansyah saya seorang anak yang dibesarkan dilingkungan seni. Seni mengalir di darah saya sedari lahir karna Nenek saya adalah seorang pelakon seni di Jakarta. Saya juga seorang penyanyi, khususnya penyanyi lagu-lagu daerah (Sunda). Saya juga seorang freelance model/talent/muse untuk beberapa produk baju, film pendek, iklan, serta penyambutan tamu diberbagai acara dan lain-lain." />
+      <meta property="og:description" content="Gesly Ardiansyah adalah seorang anak yang dibesarkan dilingkungan seni. Seni mengalir di darah saya sedari lahir karna Nenek saya adalah seorang pelakon seni di Jakarta. Saya juga seorang penyanyi, khususnya penyanyi lagu-lagu daerah (Sunda). Saya juga seorang freelance model/talent/muse untuk beberapa produk baju, film pendek, iklan, serta penyambutan tamu diberbagai acara dan lain-lain." />
       <meta name="keywords" content="Gesly Ardiansyah, Gesly, Ages, Model Bandung" />
       <meta name="robots" content="index, follow" />
+      <link rel="canonical" href="https://www.geslyardiansyah.site/" />
       <body className="antialiased">
         <Main />
         <NextScript />
